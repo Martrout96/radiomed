559e64f3b55d185aed3bb5d1b536eeb1e9d6ba3d
Cotel <miguelcoletomunoz@hotmail.com>
2017-02-28 11:09:04 +0100

    Correccion renderizacion lista

--- src/RadiomedApp.js
+++ src/RadiomedApp.js
@@ -4,16 +4,25 @@ import {
     Text,
     View
 } from 'react-native';
-import {Container, Content} from 'native-base';
-import {List} from './common/list';
+import {Container,Header, Title, Content} from 'native-base';
+import List from './common/list';
+
+const signos = [
+    {name: "Panalizacion", id: 0},
+    {name: "Consolidacion", id: 1},
+    {name: "Enfisema", id: 2},
+]
 
 export default class Radiomed extends Component {
+
     render() {
         return (
             <Container>
+                <Header>
+                    <Title>Prueba</Title>
+                </Header>
                 <Content>
-                    <List items={[]}/>
-
+                    <List items={signos}/>
                 </Content>
             </Container>
         );
--- src/common/list.js
+++ src/common/list.js
@@ -2,21 +2,18 @@ import React, {Component} from 'react';
 import {List, ListItem, Text} from 'native-base';
 
 export default class MyList extends Component{
-    renderItems(){
-        this.props.items.map((item)=>{
-            return(
-                <ListItem key={item}>
-                    <Text> {item.name}</Text>
-                </ListItem>
-            )
-        });
+    renderItems(item){
+        return (
+            <ListItem key={item.id}>
+                <Text>{item.name}</Text>
+            </ListItem>
+        )
     }
 
     render(){
-
         return (
             <List>
-                {this.renderItems()}
+                {this.props.items.map(this.renderItems)}
             </List>
         )
     }
